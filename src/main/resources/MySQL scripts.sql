ec3c93bd3ff2693b44bed145f21ef55e754fa7b1
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-03-21 23:11:05 +0200

    Modifying project's pom xml file. closes #2

--- src/main/resources/MySQL scripts.sql
+++ src/main/resources/MySQL scripts.sql
@@ -5,7 +5,7 @@ create database projectdb
 use projectdb
 
 /* Query for creating studenttab table in projectdb schema on MySQL DB */
-create table studenttab(
+create table studenttab (
 id int PRIMARY KEY AUTO_INCREMENT,
 sname varchar(20),
 scourse varchar(30),
@@ -14,3 +14,11 @@ sfee int
 
 /* Query for retrieving all data from studenttab table in projectdb schema on MySQL DB */
 select * from studenttab
+
+/* Query for creating location table in projectdb schema on MySQL DB */
+create table location (
+id int PRIMARY KEY,
+code varchar(20),
+name varchar(20),
+type varchar(10)
+)
